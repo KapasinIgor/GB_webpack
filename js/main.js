e38e505c0099cebed6402e7d8c9fd0f2d60074cd
KapasinIgor <kapasin.igor@gmail.com>
2022-06-01 01:02:01 +0300

    complete tasks of the lesson2

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import {diffDates, diffToHtml} from "./datecalc.js";
+import {formatError} from "./utils.js";
+import "./switcher.js";
+import "./timer.js";
+import "../style.css"
+
+
+const dateCalcForm = document.getElementById("dateCalc");
+const dateCalcResult = document.getElementById("dateCalc__result");
+
+dateCalcForm.addEventListener("submit", handleCalcDates);
+
+function handleCalcDates(event) {
+    dateCalcResult.innerHTML = "";
+    event.preventDefault();
+
+    let {firstDate, secondDate} = event.target.elements;
+    firstDate = firstDate.value, secondDate = secondDate.value;
+
+    if (firstDate && secondDate) {
+        const diff = diffDates(firstDate, secondDate);
+        dateCalcResult.innerHTML = diffToHtml(diff);
+    } else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля");
+}
